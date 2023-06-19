b82b4ba91cf0fdbb5ebb6d6af37ddcd76eb328aa
KimKW1007 <face676934@gmail.com>
2023-06-19 13:42:32 +0900

    feat : unique username 및 오류메세지 설정

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { Injectable } from "@nestjs/common";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common/exceptions";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 
@@ -12,6 +13,14 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto:AuthCredentialsDto):Promise<void>{
     const {username, password} = authCredentialsDto;
     const user = this.create({username, password})
-    await this.save(user)
+    try{
+      await this.save(user)
+    }catch(e){
+      if(e.code === '23505'){
+        throw new ConflictException('이미 사용 중인 아이디입니다.')
+      }else{
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
